1b1ff374cdf2ce12ae3318a920ce2a8bd7c739c7
성영준 <69851018+yxun20@users.noreply.github.com>
2025-07-20 18:26:08 +0900

    feat: 회원가입 기능 구현

--- crud.py
+++ crud.py
@@ -1,7 +1,11 @@
 
+
 from sqlalchemy.orm import Session
 import models
 import schemas
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
@@ -9,12 +13,23 @@ def get_user(db: Session, user_id: int):
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+def get_user_by_phone_number(db: Session, phone_number: str):
+    return db.query(models.User).filter(models.User.phone_number == phone_number).first()
+
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
 def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + "notreallyhashed"
-    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    hashed_password = pwd_context.hash(user.password)
+    db_user = models.User(
+        username=user.username,
+        phone_number=user.phone_number,
+        email=user.email,
+        hashed_password=hashed_password
+    )
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
