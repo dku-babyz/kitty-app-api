48b213ac0cc62388c9de89a4fc38282ef69a8c09
성영준 <69851018+yxun20@users.noreply.github.com>
2025-07-20 18:26:11 +0900

    feat: 회원가입 기능 구현

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
 from sqlalchemy.orm import relationship
 from database import Base
 import datetime
@@ -8,11 +8,13 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(255), unique=True, index=True)
+    phone_number = Column(String(255), unique=True, index=True)
     email = Column(String(255), unique=True, index=True)
     hashed_password = Column(String(255))
     experience_points = Column(Integer, default=0)
     level = Column(Integer, default=1)
-    is_active = Column(Integer, default=1)
+    is_active = Column(Boolean, default=True)
 
     messages = relationship("Message", back_populates="owner")
 
